1b24f96d93219a947708a74b03fc24dff7847c72
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-02-26 12:25:40 -0800

    import re_path to urls.py

--- Globomantics/globomantics/store/urls.py
+++ Globomantics/globomantics/store/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 
 from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	repath(r'^\d+', views.detail, name='detail'),
+	re_path(r'^\d+', views.detail, name='detail'),
 
 ]
