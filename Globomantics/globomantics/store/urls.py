f5a519a35dad017dc5eac605c7b04d72a9f22ea7
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-03-03 17:03:41 -0800

    re organize code to class based view class Electronics in views.py

--- Globomantics/globomantics/store/urls.py
+++ Globomantics/globomantics/store/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	re_path(r'^\d+', views.detail, name='detail'),
-	re_path(r'^electronics', views.electronics, name='electronics')
+	#re_path(r'^electronics', views.electronics, name='electronics')
+	re_path(r^'electronics', ElectronicsView.as_view(), name='electronics'),
 
 ]
--- Globomantics/globomantics/store/views.py
+++ Globomantics/globomantics/store/views.py
@@ -1,6 +1,7 @@
 from django.core.exceptions import ViewDoesNotExist
 from django.shortcuts import render
 from django.urls import path
+from django.views.generic import View
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.cache import cache_page
@@ -18,11 +19,11 @@ def detail(request):
 @csrf_exempt
 @cache_page(900)
 @require_http_methods(["GET"])
-def electronics(request):
 
-	items = ("Windows PC", "Apple Mac", "Apple iphone", "Lenovo", "Samsung", "Google")
-	
-	if request.method == 'GET':
+class ElectronicsView(View):
+
+	def get(self, request):
+		items = ("Windows PC", "Apple Mac", "Apple iphone", "Lenovo", "Samsung", "Google")
 		paginator = Paginator(items, 2)
 		pages = request.GET.get('page', 1)
 		
@@ -32,5 +33,4 @@ def electronics(request):
 			items = paginator.page(1)
 		return render(request, 'store/list.html', {'items':items})
 
-	elif request.method == 'POST':
-		return HttpResponseNotFound("Post method is not allowed")
+	
