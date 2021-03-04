3f6cb1266657f378f7828580c92ea8148b1945f2
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-03-03 17:08:17 -0800

    fix typo in urls.py

--- Globomantics/globomantics/store/urls.py
+++ Globomantics/globomantics/store/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	re_path(r'^\d+', views.detail, name='detail'),
 	#re_path(r'^electronics', views.electronics, name='electronics')
-	re_path(r^'electronics', ElectronicsView.as_view(), name='electronics'),
+	re_path(r'^electronics', ElectronicsView.as_view(), name='electronics'),
 
 ]
