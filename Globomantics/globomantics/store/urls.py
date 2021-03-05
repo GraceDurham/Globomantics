f50edddb29f638dbe359a456d2a5e00343074e3b
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-03-04 17:56:12 -0800

    add template view and class ElectronicsView2 to views.py, map view2 to url config

--- Globomantics/globomantics/store/urls.py
+++ Globomantics/globomantics/store/urls.py
@@ -2,12 +2,12 @@ from django.urls import path, re_path
 
 
 from . import views
-from .views import ElectronicsView
+from store.views import ElectronicsView2
 
 urlpatterns = [
 	path('', views.index, name='index'),
 	re_path(r'^\d+', views.detail, name='detail'),
 	#re_path(r'^electronics', views.electronics, name='electronics')
-	re_path(r'^electronics', ElectronicsView.as_view(), name='electronics'),
+	re_path(r'^electronics', ElectronicsView2.as_view(), name='electronics'),
 
 ]
--- Globomantics/globomantics/store/views.py
+++ Globomantics/globomantics/store/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ViewDoesNotExist
 from django.shortcuts import render
 from django.urls import path
-from django.views.generic import View
+from django.views.generic.base import View, TemplateView
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.cache import cache_page
@@ -26,11 +26,22 @@ class ElectronicsView(View):
 		items = ("Windows PC", "Apple Mac", "Apple iphone", "Lenovo", "Samsung", "Google")
 		paginator = Paginator(items, 2)
 		pages = request.GET.get('page', 1)
-		
+
+
 		try:
-			items = paginator.page(pages)
+
+			items= paginator.pages(pages)
 		except PageNotAnInteger:
-			items = paginator.page(1)
-		return render(request, 'store/list.html', {'items':items})
+			items= paginator.page(1)
+
+		return render(request, 'store/list.html', {'items': items})
+
+class ElectronicsView2(TemplateView):
+	template_name = "store/list.html"
+
+	def get_context_data(self, **kwargs):
+		items = ("Windows PC", "Apple Mac", "Apple iphone", "Lenovo", "Samsung", "Google")
+		context = {'items': items}
+		return context
 
 	
