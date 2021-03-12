e8f350be6907a22b12b4d12bbd561b53c6013e89
Grace Durham <gracedurham@Graces-MacBook-Pro.local>
2021-03-12 15:28:29 -0800

    Tie logout link to logout path in urls.py

--- Globomantics/globomantics/store/urls.py
+++ Globomantics/globomantics/store/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
 	path('', views.index, name='index'),
 	re_path(r'^\d+', views.detail, name='detail'),
 	re_path(r'^electronics', views.electronics, name='electronics'),
+	re_path(r'^logout', views.logout, name='logout')
 	# re_path(r'^electronics', ElectronicsView2.as_view(), name='electronics'),
 	# re_path(r'^electronics', ElectronicsView3.as_view(), name='electronics')
 	# re_path(r'^electronics', EquipmentView.as_view(), name='electronics'),
